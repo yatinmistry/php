51102c2f9a550a3ed3dcbcafb970f9e80c0ea42c
yatinmistry <developer.yatin.m@gmail.com>
2018-05-25 19:43:10 +0530

    Update Functions.php

--- Functions.php
+++ Functions.php
@@ -305,3 +305,8 @@ function string_after_number($str){
     }
     return substr($str,$i,$ln);
 }
+
+function jsonPretty($data){
+	$data = !is_array($data)?json_decode($data):$data;
+	return "<pre style='border:0;background-color:transparent;'>".json_encode($data,JSON_PRETTY_PRINT)."</pre>";
+}
